59d9e55f904f490e1c2000d221cf74c9087dbefa
lanaflon <lanaflonpro@gmail.com>
2019-03-05 13:19:53 +0100

    ajout

---
+++ panier-dao/src/main/java/afpa/cdi/elastique/panier/dao/App.java
@@ -0,0 +1,13 @@
+package afpa.cdi.elastique.panier.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
